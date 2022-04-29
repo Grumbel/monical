c1bdbf82b888f0aa24f5581326ced6ae43122d22
Ingo Ruhnke <grumbel@gmail.com>
2022-04-29 14:03:41 +0200

    Add Nix flake build file

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree rec {
+          monical = pkgs.python3Packages.buildPythonPackage rec {
+            name = "monical";
+            src = self;
+            propagatedBuildInputs = [
+              pkgs.python3Packages.pygame
+            ];
+          };
+        };
+        defaultPackage = packages.monical;
+      });
+}
+
