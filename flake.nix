45bed0353e9b4b150af9b9f7a4656a62651c4db0
Ingo Ruhnke <grumbel@gmail.com>
2023-05-27 11:33:18 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -9,19 +9,29 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in rec {
-        packages = flake-utils.lib.flattenTree rec {
+        packages = rec {
+          default = monical;
+
           monical = pkgs.python3Packages.buildPythonPackage rec {
-            name = "monical";
-            src = self;
-            meta = {
-              mainProgram = "monical";
-            };
-            propagatedBuildInputs = [
-              pkgs.python3Packages.pygame
+            pname = "monical";
+            version = "0.0.0";
+
+            src = ./.;
+
+            propagatedBuildInputs = with pkgs; [
+              python3Packages.pygame
             ];
           };
         };
-        defaultPackage = packages.monical;
-      });
+
+        apps = rec {
+          default = monical;
+          monical = flake-utils.lib.mkApp {
+            drv = packages.monical;
+            exePath = "/bin/monical";
+          };
+        };
+      }
+    );
 }
 
